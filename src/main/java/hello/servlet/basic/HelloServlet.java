a48fb1452439e8b7d369fb6836473e55c6436b75
Bae Seong Whan <rlglxh@naver.com>
2023-01-21 20:22:42 +0900

    GET

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -21,6 +21,6 @@ public class HelloServlet extends HttpServlet {
 
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write("hello " + username);
+       response.getWriter().write("hello" + username);
     }
 }
